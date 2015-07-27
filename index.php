c12e14459fa7564fd6fd5ea75b300cbaf439f8a2
unknown <ahmet.ddu@gmail.com>
2015-07-27 18:07:52 +0300

    hata duzeltimesi

--- index.php
+++ index.php
@@ -9,31 +9,6 @@ date_default_timezone_set('Europe/Istanbul'); //Turkiye saati
 $day_now = date('N'); //Suanki gun degeri
 $hour_now = date('H:i'); //Suanki saat degeri
 
-//if($hour < $days[$day]['morning_start'] || $hour > $days[$day]['afternoon_finish'] ){
-//
-//    //Mesai saatleri disi
-//
-//
-//
-//
-//}elseif($hour > $days[$day]['morning_finish'] && $hour < $days[$day]['afternoon_start'] ){
-//
-//    //Oglen arasi
-//
-//    $bfxm = new Builder();
-//    $bfxm->dial($launch_break)
-//        ->build(true);
-//
-//}else{
-//
-//    //Mesai saatleri
-//
-//    $bfxm = new Builder();
-//    $bfxm->dial($shift_in)
-//        ->build(true);
-//
-//}
-
 switch($hour_now){
     //Mesai saatleri disi
     case ($hour_now < $days[$day_now]['morning_start'] || $hour_now > $days[$day_now]['afternoon_finish'] ):
